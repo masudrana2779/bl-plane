31d498af756ea6d0f09a0748473e2b24c8006732
masudrana2779 <masudrana2779@gmail.com>
2022-10-11 16:06:10 +0600

    try to done

--- Components/Home/Home.styled.tsx
+++ Components/Home/Home.styled.tsx
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 
 export const BlPlanWrap = styled.div`
-  padding: 20px 0;
+  padding: 20px 0 200px;
+  position: relative;
+
   h4 {
     text-align: center;
     margin-bottom: 40px;
@@ -48,6 +50,7 @@ export const BlPlanWrap = styled.div`
           justify-content: center;
           width: 48px;
           height: 48px;
+          font-size: 10px;
           border-radius: 50%;
           border: 1px solid rgba(0, 0, 0, 0.12);
           margin: 0;
@@ -60,8 +63,139 @@ export const BlPlanWrap = styled.div`
     }
   }
   .Total {
+    background: #f5f5f5;
+    width: 100%;
+    padding: 0;
+    margin: 0;
+    position: fixed;
+    z-index: 99;
+    bottom: 0;
+    left: 0;
+    right: 0;
     .right-sidebar-container {
+      padding: 18px 12px 16px 12px;
+      display: flex;
+      flex-direction: row;
+      flex-wrap: nowrap;
+      align-items: flex-start;
+      width: 100%;
+      margin: auto;
       .right-sidebar {
+        width: 28%;
+        padding-top: 6px;
+        .myob_bonus_num {
+          font-size: 27px;
+          line-height: 20px;
+          font-weight: 700;
+          font-style: italic;
+          color: #000;
+          margin-bottom: 2px;
+          .myob_bonus_text {
+            font-size: 14px;
+            line-height: 16px;
+            padding: 3px 0 0 0;
+            font-weight: 500;
+            display: block;
+            margin: 0;
+            color: #000;
+          }
+        }
+      }
+      .sidebar-purchage {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: nowrap;
+        width: 72%;
+        justify-content: space-between;
+        align-items: center;
+        #myob_bttom_stky_left {
+          width: 65%;
+
+          .myob_sidebar {
+            p {
+              padding: 0;
+              font-weight: 500;
+              margin-bottom: 0;
+              text-align: left;
+              font-size: 17px;
+              line-height: 21px;
+              padding: 0;
+              font-weight: 500;
+              &.flxpln-sml-terms {
+                font-size: 13px;
+                line-height: 20px;
+                color: #767676;
+                padding-top: 3px;
+              }
+              &.purchase_title {
+                font-size: 12px;
+                line-height: 20px;
+                color: #767676;
+                font-weight: 400;
+                display: flex;
+                flex-wrap: wrap;
+                justify-content: flex-start;
+                span {
+                  &:first-child {
+                    margin-left: 0;
+                  }
+                  font-size: 11px;
+                  line-height: 20px;
+                  padding-bottom: 0;
+                  font-weight: 500;
+                  color: #767676;
+                  position: relative;
+                  display: flex;
+                  align-items: center;
+                  &:after {
+                    content: "|";
+                    margin: 0 5px;
+                  }
+                }
+              }
+            }
+          }
+        }
+        .btn {
+          display: inline-block;
+          font-weight: 400;
+          color: #212529;
+          text-align: center;
+          vertical-align: middle;
+          -webkit-user-select: none;
+          -moz-user-select: none;
+          -ms-user-select: none;
+          user-select: none;
+          background-color: transparent;
+          border: 1px solid transparent;
+          padding: 0.375rem 0.75rem;
+          font-size: 1rem;
+          line-height: 1.5;
+          border-radius: 0.25rem;
+          transition: color 0.15s ease-in-out,
+            background-color 0.15s ease-in-out, border-color 0.15s ease-in-out,
+            box-shadow 0.15s ease-in-out;
+        }
+        .btn-flexiplan,
+        .btn-flexiplan-default {
+          font-family: Noto Sans Bengali, Telenor, Helvetica Neue, Helvetica,
+            Arial, sans-serif, Source Sans Pro;
+          background: #ff8126;
+        }
+        .btn-flexiplan {
+          border: 0;
+        }
+        .btn-flexiplan {
+          color: #fff !important;
+          font-size: 16px !important;
+          height: 40px !important;
+          line-height: 40px !important;
+          padding: 0 24px !important;
+          margin: 0;
+        }
+        .btn:not(:disabled):not(.disabled) {
+          cursor: pointer;
+        }
       }
     }
   }
--- Components/Home/index.tsx
+++ Components/Home/index.tsx
@@ -1,51 +1,57 @@
+import { useState } from "react";
 import { BlPlanWrap } from "./Home.styled";
 
 const data = [
   {
     packTitle: "Validity",
+    pack: ["1", "3", "7"],
   },
   {
-    packTitle: "4G 1 Internet",
+    packTitle: "Internet",
+    pack: ["0 MB", "205 MB", "500 MB"],
   },
   {
     packTitle: "Minutes",
-  },
-  {
-    packTitle: "Bioscope",
+    pack: ["0", "10", "25"],
   },
   {
     packTitle: "SMS",
+    pack: ["0", "50", "100"],
   },
 ];
 
 const HomeComponent = () => {
+  const [validity, setValidity] = useState();
+  const [internet, setInternet] = useState();
+  const [minutes, setMinutes] = useState();
+  const [SMS, setSMS] = useState();
   return (
     <BlPlanWrap className="blPlanWrap">
       <div className="container">
-        <h4>BL Plan</h4>
+        <h4>Banglalinnk Offer</h4>
         <div className="row justify-content-center">
           <div className="col-md-8 col-sm-12 col-12">
-            {data.map((pack: any, k: number) => (
+            {data.map((dataItem: any, k: number) => (
               <div key={k} className="planContent">
                 <div className="row">
                   <div className="col-5">
-                    <h3>{pack.packTitle}</h3>
+                    <h3>{dataItem.packTitle}</h3>
                     <p>3 Days</p>
                   </div>
                   <div className="col-7">
                     <div className="packItemWrap">
-                      {[1, 1, 1, 1, 1, 1, 1].map((item: any, i: number) => (
+                      {dataItem.pack.map((item: any, i: number) => (
                         <div className="packItem" key={i + k}>
                           <input
-                            name="day"
+                            name={dataItem.packTitle}
                             type="radio"
-                            id={`item_day_${k}${i}`}
+                            id={`item_${k}${i}`}
                           />
                           <label
-                            htmlFor={`item_day_${k}${i}`}
+                            htmlFor={`item_${k}${i}`}
                             className="packData"
                           >
-                            0
+                            {item}
                           </label>
                         </div>
                       ))}
@@ -57,36 +63,33 @@ const HomeComponent = () => {
           </div>
           <div className="Total">
             <div className="right-sidebar-container">
-              <div className="right-sidebar row">
-                <div className="myob-color-stripe col-3">
-                  <div>
-                    <h2 className="myob_bonus_num">
-                      <span id="discount-amount">10</span>
-                      <span className="myob_percent">%</span>
-                    </h2>
-                    <h6 className="myob_bonus_text">Savings</h6>
+              <div className="right-sidebar">
+                <div>
+                  <h2 className="myob_bonus_num">
+                    <span id="discount-amount">10</span>
+                    <span className="myob_percent">%</span>
+                  </h2>
+                  <h6 className="myob_bonus_text">Savings</h6>
+                </div>
+              </div>
+              <div className="sidebar-purchage">
+                <div id="myob_bttom_stky_left">
+                  <div className="myob_sidebar">
+                    <p className="flxpln-sml-terms">Total Price</p>
+                    <p>
+                      ৳ <span id="bundle-price">18.00</span>
+                    </p>
+                    <p className=" purchase_title">
+                      <span className="longevity">1 Days</span>
+                      <span className="voice">25 Min</span>
+                      <span className="internet">0 MB</span>
+                      <span className="fourg">0 MB 4G Only </span>
+                      <span className="bioscope">0 MB Bioscope </span>
+                      <span className="sms">0 SMS</span>
+                    </p>
                   </div>
                 </div>
-                <div className="sidebar-purchage col-9">
-                  <div id="myob_bttom_stky_left">
-                    <div className="myob_sidebar">
-                      <p className="flxpln-sml-terms">Total Price</p>
-                      <p>
-                        {" "}
-                        ৳ <span id="bundle-price">18.00</span>
-                      </p>
-                      <p className="myob_smll_device_selected_items purchase_title purchase_list order-seq-tw">
-                        <span className="longevity">1 Days</span>
-                        <span className="voice">25 Min</span>
-                        <span className="internet">0 MB</span>
-                        <span className="fourg">0 MB 4G Only </span>
-                        <span className="bioscope">0 MB Bioscope </span>
-                        <span className="sms">0 SMS</span>
-                      </p>
-                    </div>
-                  </div>
-                  <button className="btn btn-flexiplan">Buy</button>
-                </div>
+                <button className="btn btn-flexiplan">Buy</button>
               </div>
             </div>
           </div>
